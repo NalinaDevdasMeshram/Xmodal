9dd3e09b2938d92976bbc703679a8e1ad255e081
nalina <naleenameshram85@gmail.com>
2024-08-11 19:47:29 +0530

    working

---
+++ Modals/src/App.jsx
@@ -0,0 +1,12 @@
+import Modals from "./components/Modals";
+
+function App() {
+  return (
+    <div>
+      <h1 style={{ textAlign: "center" }}>User Details</h1>
+      <Modals />
+    </div>
+  );
+}
+
+export default App;
